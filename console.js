fa74af6aa9ca35128d928e13a5d364b3ebcfef95
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2018-12-12 16:26:09 +0900

    fix typo

--- console.js
+++ console.js
@@ -1,4 +1,30 @@
 function consolist() {
-    console.log("love");
+    // とにかく出す
+    console.log("hey");
+    // 色付けたり太くしたり(スタイル指定)
+    console.log("%chey", 'color: red;');
+    console.log("%chey", 'color: red; font-weight: bold');
+    // レベル分け
+    console.debug("hey");
+    console.info("hey");
+    console.warn("hey");
+    console.error("hey");
+    // 変数値埋め込み
+    const a = "へいほー";
+    console.log(`hey${a}`);
+    // オブジェクト内容全出し
+    const aa = { a: 1, b: 2, c: { d: "hey" } }
+    console.log(JSON.stringify(aa));
+    // オブジェクト、クリックで掘れる形に表示
+    const aaa = { a: 1, b: 2, c: { d: "hey" } }
+    console.dir(aaa);
+    // debugモードの時だけ表示したいとか
+    let debug = true; // 何某かで設定。
+    window.mylog = debug ? console.log.bind(console) : () => { };
+    window.mylog("hey");
+
+    debug = false; // 何某かで設定。
+    window.mylog = debug ? console.log.bind(console) : () => { };
+    window.mylog("hey");
 }
 consolist();
